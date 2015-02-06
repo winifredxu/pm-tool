a6fb08ffb9bdeb2d949f55b581bc3583e81a1c22
Cal <bcflow@riseup.net>
2015-02-06 12:29:06 -0800

    Fixed Tasks tables

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,63 +1,46 @@
 class TasksController < ApplicationController
   before_action :find_project,
-   only: [:create, :show, :destroy, :search, :edit]
+    only: [:create, :show, :destroy, :search, :edit]
   before_action :authenticate_user!
 
   def create
     @task = @project.tasks.new task_params
-    if @task.save
-      redirect_to @project, notice: "Task created successfully"
-    else
-      redirect_to @project
-      #render "projects/show.html.erb"
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @project, notice: "Task created successfully!"}
+        format.js   { render }
+      else
+        format.html { render "projects/show"}
+        format.js { render }
+        #redirect_to @project
+        #render "projects/show.html.erb"
+      end
     end
   end
-
-  # def create
-  #   @task = @project.tasks.new task_params
-  #   #respond_to do |format|
-  #   if @task.save
-  #     redirect_to @project
-  #     #format.html { redirect_to @project }
-  #     #format.js { render }
-  #   else
-  #     render "projects/show/"
-    
-  #   #   respond_to do |format|
-  #   #     format.html { redirect_to @project, notice: "Task created successfully" }
-  #   #     format.js { render }
-  #   #   end 
-  #   # else
-  #   #   format.js { render }
-
-
-  #   end
-
-
 
   def destroy
     task = Task.find params[:id]
     @project = Project.friendly.find task.project_id
-    
+
     task.destroy
     flash[:success] = "Task was successfully deleted."
-    
+
     respond_to do |format|
       format.html { redirect_to @project }
-      format.js { render }
+      format.js   { render }
     end
   end
 
   def edit
-    @task = Task.find params[:id]
+    @task = @project.tasks.find params[:id]
   end
 
   def update
     @task = Task.find params[:id]
     @project = Project.friendly.find @task.project_id
     if @task.update task_params
-      redirect_to project_path(@task.project_id)
       flash[:success] = "Task was successfully edited."
+      redirect_to project_path(@task.project_id)
     else
       render :edit
     end
